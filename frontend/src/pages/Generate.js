6163b59ee882c054d5fbf8a0d201889b6c29dc46
Antonio Gómez <antonio.gomez21@alu.uclm.es>
2024-12-30 01:20:32 +0100

    feat: add components to Generate page

--- frontend/src/pages/Generate.js
+++ frontend/src/pages/Generate.js
@@ -1,16 +1,19 @@
-import Navbar from "../componentes/Navbar";
-import ProgramInput from "../componentes/ProgramInput";
+import Navbar from "../components/Navbar";
 import icon_image from "../assets/GeoCraft_icon_light.png";
+import CodeEditor from "../components/CodeEditor";
 
 const Generate = () => {
     return (
         <div className="page-container">
             <Navbar />
             <div className="content-container">
-                <div class="fixed-image">
+                <div className="fixed-image">
                     <img src={icon_image} alt="Icon" />
                 </div>
-                <ProgramInput />
+                <div className="editor-container">
+                    <CodeEditor />
+                    <button className="generate-button">Generate</button>
+                </div>
             </div>
 
         </div>
