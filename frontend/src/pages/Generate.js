406134a7a7f91f8d7479a2950503f2b713d3cbe5
Antonio Gómez <antonio.gomez21@alu.uclm.es>
2025-01-01 18:23:43 +0100

    feat: update generate page

--- frontend/src/pages/Generate.js
+++ frontend/src/pages/Generate.js
@@ -3,20 +3,30 @@ import icon_image from "../assets/GeoCraft_icon_light.png";
 import CodeEditor from "../components/CodeEditor";
 
 const Generate = () => {
+
+    const handleGenerateClick = () => {
+        // Obtain the code from the CodeEditor component
+        let code = document.querySelector(".code-editor-textarea").value;
+        console.log(code);
+    }
+
+
     return (
         <div className="page-container">
             <Navbar />
-            <div className="content-container">
+            <div className="generate-content-container">
                 <div className="fixed-image">
                     <img src={icon_image} alt="Icon" />
                 </div>
                 <div className="editor-container">
+                    <h1>Generate Unity World</h1>
                     <CodeEditor />
-                    <button className="generate-button">Generate</button>
+                    <button className="generate-button"
+                        onClick={handleGenerateClick}>Generate</button>
                 </div>
             </div>
 
-        </div>
+        </div >
     );
 };
 
